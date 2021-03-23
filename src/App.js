67b472684cd5b0c9047ba678114c3cc9b2aba895
yh10106699 <yyang153@hawk.iit.edu>
2021-03-23 13:00:37 -0500

    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from "axios";
+const axiosGithubGraphQL = axios.create({
+    baseURL: 'https://api.github.com/graphql',
+    headers: {
+        Authorization: "bearer 85f2ad5d40480d165ac790d19800a370151b3b72",
+    }
+})
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TITLE = 'React GraphQL GitHub Client';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            path: 'the-road-to-learn-react/the-road-to-learn-react',
+        };
+    }
+
+    componentDidMount() {
+        // fetch data
+    }
+
+    onChange = event => {
+        this.setState({ path: event.target.value });
+        //the path in the state is set according to user input
+    };
+
+    onSubmit = event => {
+        // fetch data
+        event.preventDefault();
+    };
+
+    render() {
+        const { path } = this.state;
+        return (
+            <div>
+                <h1>{TITLE}</h1>
+                <form onSubmit={this.onSubmit}>
+                    <label htmlFor="url">
+                        Show open issues for https://github.com/
+                    </label>
+                    <input
+                        id="url"
+                        type="text"
+                        value = {path}
+                        onChange={this.onChange}
+                        style={{ width: '300px' }}
+                    />
+                    <button type="submit">Search</button>
+                </form>
+                <hr />
+            </div>
+        );
+    }
 }
-
 export default App;
